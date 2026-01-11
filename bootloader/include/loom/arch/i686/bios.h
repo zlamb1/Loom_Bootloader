1a3f4d30e9ea542871960e2be1fef54946074a66
zlamb1 <102142742+zlamb1@users.noreply.github.com>
2026-01-10 18:40:44 -0600

    created bios int args struct

--- bootloader/include/loom/arch/i686/bios.h
+++ bootloader/include/loom/arch/i686/bios.h
@@ -1,4 +1,20 @@
 #ifndef LOOM_I686_BIOS_H
 #define LOOM_I686_BIOS_H 1
 
+#include "loom/compiler.h"
+#include "loom/types.h"
+
+typedef struct
+{
+  loom_u32 eax;
+  loom_u32 ebx;
+  loom_u32 ecx;
+  loom_u32 edx;
+  loom_u32 esi;
+  loom_u32 edi;
+  loom_u16 flags;
+  loom_u16 ds;
+  loom_u16 es;
+} PACKED loom_bios_args_t;
+
 #endif
--- bootloader/include/loom/compiler.h
+++ bootloader/include/loom/compiler.h
@@ -2,6 +2,7 @@
 #define LOOM_COMPILER_H 1
 
 #define NORETURN     __attribute__ ((noreturn))
+#define PACKED       __attribute__ ((packed))
 #define PRINTF(X, Y) __attribute__ ((format (printf, X, Y)))
 #define UNUSED       __attribute__ ((unused))
 
