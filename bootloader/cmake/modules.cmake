bc970446c4e047ea68cb53e2dd5ad4dec101cec3
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2026-02-17 01:25:02 -0600

    Added initial FAT module with type definitions. More work must be done on FS registration and file reading.

--- bootloader/cmake/modules.cmake
+++ bootloader/cmake/modules.cmake
@@ -1,2 +1,4 @@
 # Core Modules
+
+create_module(fat ${LOOM_MOD_SRC_DIR}/fat.c)
 create_module(hello ${LOOM_MOD_SRC_DIR}/hello.c)
---
+++ bootloader/src/mods/fat.c
@@ -0,0 +1,99 @@
+#include "loom/module.h"
+
+typedef struct
+{
+#define FAT_EBPB16                                                            \
+  loom_uint8_t drive_num;                                                     \
+  loom_uint8_t nt_flags;                                                      \
+  loom_uint8_t sig;                                                           \
+  loom_uint32_t volume_id;                                                    \
+  char volume_label[11];                                                      \
+  char sys_ident[8];
+#define FAT_EBPB_SIG1 0x28
+#define FAT_EBPB_SIG2 0x29
+  FAT_EBPB16
+} LOOM_PACKED fat_ebpb16_t;
+
+typedef struct
+{
+  loom_uint32_t fatsz32;
+  loom_uint16_t flags;
+  loom_uint16_t version;
+  loom_uint32_t root_cluster;
+  loom_uint16_t fsinfo_sect;
+  loom_uint16_t backup_sect;
+  char res1[12];
+  FAT_EBPB16
+} LOOM_PACKED fat_ebpb32_t;
+
+typedef struct
+{
+  loom_uint8_t jmp[3];
+  char oem_ident[8];
+  loom_uint16_t bytes_per_sect;
+  loom_uint8_t sects_per_cluster;
+  loom_uint16_t start_sectors;
+  loom_uint8_t fats;
+  loom_uint16_t root_dents;
+  loom_uint16_t sects16;
+  loom_uint8_t media_desc;
+  loom_uint16_t fatsz16;
+  loom_uint16_t sects_per_track;
+  loom_uint16_t heads;
+  loom_uint32_t hidden_sects;
+  loom_uint32_t sects32;
+
+  union
+  {
+    fat_ebpb16_t ebpb16;
+    fat_ebpb32_t ebpb32;
+  };
+} LOOM_PACKED fat_bpb_t;
+
+typedef struct
+{
+#define FAT_FSINFO_SIG1 0x41615252
+  loom_uint32_t sig1;
+  char res1[480];
+#define FAT_FSINFO_SIG2 0x61417272
+  loom_uint32_t sig2;
+  loom_uint32_t clusters_hint;
+  loom_uint32_t free_cluster_hint;
+  char res2[12];
+#define FAT_FSINFO_SIG3 0xAA550000
+  loom_uint32_t sig3;
+} LOOM_PACKED fat_fsinfo_t;
+
+typedef struct
+{
+  char filename[11];
+#define FAT_FILE_ATTRIB_RDONLY    0x1
+#define FAT_FILE_ATTRIB_HIDDEN    0x2
+#define FAT_FILE_ATTRIB_SYSTEM    0x4
+#define FAT_FILE_ATTRIB_VOLUME_ID 0x8
+#define FAT_FILE_ATTRIB_DIR       0x10
+#define FAT_FILE_ATTRIB_ARCHIVE   0x20
+  loom_uint8_t attribs;
+  char res1;
+  loom_uint8_t ctime_subsec;
+#define FAT_FILE_TIME_HOUR(X) ((X) & 0b11111)
+#define FAT_FILE_TIME_MIN(X)  ((X) & 0b11111100000)
+#define FAT_FILE_TIME_SEC(X)  (((X) & 0b1111100000000000) * 2)
+  loom_uint16_t ctime;
+#define FAT_FILE_DATE_YEAR(X)  ((X) & 0b1111111)
+#define FAT_FILE_DATE_MONTH(X) ((X) & 0b11110000000)
+#define FAT_FILE_DATE_DAY(X)   ((X) & 0b1111100000000000)
+  loom_uint16_t cdate;
+  loom_uint16_t adate;
+  loom_uint16_t cluster_hi;
+  loom_uint16_t mtime;
+  loom_uint16_t mdate;
+  loom_uint16_t cluster_lo;
+  loom_uint32_t filesz;
+} LOOM_PACKED fat_dirent_t;
+
+LOOM_MOD (fat)
+
+LOOM_MOD_INIT () {}
+
+LOOM_MOD_DEINIT () {}
