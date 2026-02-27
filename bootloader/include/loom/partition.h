52a6ab74df1a63cf8b24aa6dea933153082cf2d8
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2026-02-27 07:49:13 -0600

    Added loom_partition_init.

--- bootloader/include/loom/partition.h
+++ bootloader/include/loom/partition.h
@@ -1,6 +1,7 @@
 #ifndef LOOM_PARTITION_H
 #define LOOM_PARTITION_H 1
 
+#include "loom/assert.h"
 #include "loom/block_dev.h"
 #include "loom/partition_scheme.h"
 
@@ -15,5 +16,24 @@ extern loom_list_t loom_partition_schemes;
 loom_error_t loom_partition_read (loom_block_dev_t *block_dev,
                                   loom_usize_t block, loom_usize_t n,
                                   char *buf);
+
+static inline void
+loom_partition_init (loom_partition_t *partition, loom_block_dev_t *parent,
+                     loom_usize_t offset, loom_usize_t blocks)
+{
+  loom_assert (partition != NULL);
+  loom_assert (parent != NULL);
+
+  loom_block_dev_init_t init = {
+    .parent = parent,
+    .read = loom_partition_read,
+    .blocksz = parent->blocksz,
+    .blocks = blocks,
+    .data = partition,
+  };
+
+  loom_block_dev_init (&partition->base, &init);
+  partition->offset = offset;
+}
 
 #endif
